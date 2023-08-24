ca04c5b420bea14c58f15dd8a42ddbd29bb1af02
Alpha <alpha.likembe@student.moringaschool.com>
2023-08-24 05:08:53 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
 
+
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>I made this!</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+
+
+      <h3>Links</h3>
+      <a href="https://github.com/liza">https://github.com/liza</a>
+      <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
+
+
+      
     </div>
   );
 }
