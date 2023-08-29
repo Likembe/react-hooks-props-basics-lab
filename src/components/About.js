e37811dd3fba18c4a2dc1d003efa078f6c2db07c
Alpha <alpha.likembe@student.moringaschool.com>
2023-08-29 21:58:57 +0300

    review

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
-
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I made this!</p>
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-
-
-
-      <h3>Links</h3>
-      <a href="https://github.com/liza">https://github.com/liza</a>
-      <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
-
-
-      
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
